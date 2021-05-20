3552c87a34ea759a80598229fbf4d48c7730aa34
JLMG8 <pepeluis963@gmail.com>
2021-05-20 19:00:52 +0200

    Creación del test: testRangoAMenorQueB

--- test/main/OperacionesTest.java
+++ test/main/OperacionesTest.java
@@ -7,15 +7,20 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Ignore;
 
 
 public class OperacionesTest {
+    
+    private static Operaciones operaciones;
     
     public OperacionesTest() {
     }
     
     @BeforeClass
     public static void setUpClass() {
+        //Objeto de prueba
+        operaciones = new Operaciones();
     }
     
     @AfterClass
@@ -24,6 +29,7 @@ public class OperacionesTest {
     
     @Before
     public void setUp() {
+        
     }
     
     @After
@@ -34,21 +40,24 @@ public class OperacionesTest {
      * Test of rango method, of class Operaciones.
      */
     @Test
-    public void testRango() {
-        System.out.println("rango");
-        int a = 0;
-        int b = 0;
-        Operaciones instance = new Operaciones();
-        int[] expResult = null;
-        int[] result = instance.rango(a, b);
+    //A menor que B
+    public void testRangoAMenorQueB() {
+        
+        int a = 2;
+        int b = 8;
+        
+        int[] expResult = {2,3,4,5,6,7,8};
+        
+        int[] result = operaciones.rango(a, b);
+        
         assertArrayEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        
     }
 
     /**
      * Test of sumRango method, of class Operaciones.
      */
+    @Ignore
     @Test
     public void testSumRango() {
         System.out.println("sumRango");
@@ -65,6 +74,7 @@ public class OperacionesTest {
     /**
      * Test of isPar method, of class Operaciones.
      */
+    @Ignore
     @Test
     public void testIsPar() {
         System.out.println("isPar");
@@ -80,6 +90,7 @@ public class OperacionesTest {
     /**
      * Test of extraerPositivos method, of class Operaciones.
      */
+    @Ignore
     @Test
     public void testExtraerPositivos() {
         System.out.println("extraerPositivos");
