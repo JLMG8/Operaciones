942755dbad120b0b39ac53784869016d80741265
JLMG8 <pepeluis963@gmail.com>
2021-05-21 21:19:56 +0200

    Creación de la clase Main.

---
+++ src/main/Main.java
@@ -0,0 +1,15 @@
+
+package main;
+
+/**
+ *
+ * @author JLMG8
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        Operaciones operaciones = new Operaciones();
+        
+        
+    }
+}
